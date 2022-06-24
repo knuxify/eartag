83d7fd6ff227ec11941f5cc055f32c8c827912ae
knuxify <knuxify@gmail.com>
2022-06-24 19:37:51 +0200

    fileview: split modified editablelabels into separate file, add placeholders

--- src/fileview.py
+++ src/fileview.py
@@ -32,6 +32,65 @@ from gi.repository import Adw, Gtk, GObject, Pango
 from os.path import basename
 import re
 
+class EartagEditableLabel(Gtk.EditableLabel):
+    """
+    Editable labels are missing a few nice features that we need
+    (namely proper centering and word wrapping), but since they're
+    just GtkStacks with a regular GtkLabel inside, we can modify
+    them to suit our needs. This class automates the process.
+    """
+    __gtype_name__ = 'EartagEditableLabel'
+
+    _placeholder = ''
+
+    def __init__(self):
+        super().__init__()
+
+        # The layout is:
+        # GtkEditableLabel
+        #  -> GtkStack
+        #     -> GtkStackPage
+        #        -> GtkLabel
+        # We use "get_first_child" since that's the easiest way to get
+        # the direct child of the object (EditableLabel has no get_child).
+        label = self.get_first_child().get_pages()[0].get_child()
+        editable = self.get_first_child().get_pages()[1].get_child()
+
+        label.set_wrap(True)
+        label.set_wrap_mode(Pango.WrapMode.WORD_CHAR)
+        label.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
+        label.set_lines(3)
+        label.set_max_width_chars(128)
+        label.set_justify(Gtk.Justification.CENTER)
+        self.set_alignment(0.5)
+
+        self.bind_property('placeholder', editable, 'placeholder-text',
+            GObject.BindingFlags.SYNC_CREATE)
+
+        self.connect('notify::editing', self.display_placeholder)
+        self.connect('notify::text', self.display_placeholder)
+
+        self.label = label
+        self.editable = editable
+        self.display_placeholder()
+
+    def display_placeholder(self, *args):
+        """Displays/hides placeholder in non-editing mode as needed."""
+        if self.get_text() == '':
+            self.label.set_label(self.placeholder)
+            self.label.add_css_class('placeholder')
+        else:
+            self.label.remove_css_class('placeholder')
+
+    @GObject.Property(type=str)
+    def placeholder(self):
+        """Placeholder to display when the text is empty."""
+        return self._placeholder
+
+    @placeholder.setter
+    def placeholder(self, value):
+        self._placeholder = value
+
 class EartagTagListItem(Adw.ActionRow):
     __gtype_name__ = 'EartagTagListItem'
 
@@ -79,39 +138,10 @@ class EartagFileView(Adw.Bin):
     def __init__(self, path=None):
         """Initializes the EartagFileView."""
         super().__init__()
-        self.setup_editable_label(self.title_entry, _("Title"))
-        self.setup_editable_label(self.artist_entry, _("Artist"))
 
         self.file_path = path
         if path:
             self.load_file()
-
-    def setup_editable_label(self, parent, editable_placeholder):
-        """
-        Editable labels are missing a few nice features that we need
-        (namely proper centering and word wrapping), but since they're
-        just GtkStacks with a regular GtkLabel inside, we can modify
-        them to suit our needs. This function automates the process.
-        """
-
-        # The layout is:
-        # GtkEditableLabel
-        #  -> GtkStack
-        #     -> GtkStackPage
-        #        -> GtkLabel
-        # We use "get_first_child" since that's the easiest way to get
-        # the direct child of the object (EditableLabel has no get_child).
-        label = parent.get_first_child().get_pages()[0].get_child()
-        editable = parent.get_first_child().get_pages()[1].get_child()
-
-        label.set_wrap(True)
-        label.set_wrap_mode(Pango.WrapMode.WORD_CHAR)
-        label.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
-        label.set_lines(3)
-        label.set_max_width_chars(128)
-        label.set_justify(Gtk.Justification.CENTER)
-
-        editable.set_placeholder_text(editable_placeholder)
 
     def load_file(self):
         """Reads the file path from self.file_path and loads the file."""
