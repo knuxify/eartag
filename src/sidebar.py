df655630784b61861f21b6379c1f207799665536
knuxify <knuxify@gmail.com>
2022-08-22 18:13:40 +0200

    window, filelistitem: add button tooltips

--- src/sidebar.py
+++ src/sidebar.py
@@ -1,3 +1,31 @@
+# sidebar.py
+#
+# Copyright 2022 knuxify
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE X CONSORTIUM BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Except as contained in this notice, the name(s) of the above copyright
+# holders shall not be used in advertising or otherwise to promote the sale,
+# use or other dealings in this Software without prior written
+# authorization.
+
 from gi.repository import GObject, Gtk
 import os.path
 
@@ -45,7 +73,8 @@ class EartagFileListItem(Gtk.Box):
 
     @title.setter
     def title(self, value):
-        self.title_label.set_label(value or '(No title)')
+        # TRANSLATORS: Placeholder for file sidebar items with no title set
+        self.title_label.set_label(value or _('(No title)'))
 
 class EartagFileList(Gtk.ListView):
     """List of opened tracks."""
