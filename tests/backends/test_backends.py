004e1faffcba6fc22f8d2956529ec6a362065562
knuxify <knuxify@gmail.com>
2022-11-28 14:03:18 +0100

    tests: drop taglib tests

--- tests/backends/test_backends.py
+++ tests/backends/test_backends.py
@@ -4,14 +4,10 @@ from src.backends.file_mutagen_vorbis import EartagFileMutagenVorbis
 from src.backends.file_mutagen_mp4 import EartagFileMutagenMP4
 from src.backends.file_mutagen_id3 import EartagFileMutagenID3
 from src.backends.file_mutagen_asf import EartagFileMutagenASF
-from src.backends.file_taglib import EartagFileTagLib
 
 def test_backend_mutagen_vorbis():
     run_backend_tests(EartagFileMutagenVorbis, 'flac')
     run_backend_tests(EartagFileMutagenVorbis, 'ogg')
-
-def test_backend_taglib():
-    run_backend_tests(EartagFileTagLib, 'wav')
 
 def test_backend_mutagen_id3():
     run_backend_tests(EartagFileMutagenID3, 'mp3')
